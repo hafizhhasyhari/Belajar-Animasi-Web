1a841c3eea97394cedc501a7c84a98bd4871d189
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-18 15:00:29 +0700

    Add files via upload

---
+++ webbelajar-animasi/src/index.js
@@ -0,0 +1,131 @@
+var open = document.getElementById('hamburger');
+var changeIcon = true;
+
+open.addEventListener("click", function(){
+
+    var overlay = document.querySelector('.overlay');
+    var nav = document.querySelector('nav');
+    var icon = document.querySelector('.menu-toggle i');
+
+    overlay.classList.toggle("menu-open");
+    nav.classList.toggle("menu-open");
+
+    if (changeIcon) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+
+        changeIcon = false;
+    }
+    else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+        changeIcon = true;
+    }
+});
+
+// Declaring necessary variables to the cursor class and navlinks
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('.navlinks li');
+
+// Event listener
+window.addEventListener('mousemove', cursor);
+
+// cursor function
+// pageX and pageY give the realtime mouse coordinates on screen 
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + 'px'
+    mouseCursor.style.left = e.pageX + 'px'
+}
+
+Array.from(navLinks).forEach(link => {
+    link.addEventListener('mouseleave', () => {
+        mouseCursor.classList.remove('link-grow');
+        link.classList.remove('hovered-link');
+    })
+
+    link.addEventListener('mouseover', () => {
+        mouseCursor.classList.add('link-grow');
+        link.classList.add('hovered-link');
+    })
+});
+
+
+/**************Event JS *******************/
+$('#myCarousel').carousel({
+  interval: 3000,
+})
+
+/***************Quiz JS********************/
+function resizeInput() {
+    $(this).attr('size', $(this).val().length);
+}
+
+$('input[type="text"], input[type="email"]')
+    // event handler
+    .keyup(resizeInput)
+    // resize on page load
+    .each(resizeInput);
+
+
+console.clear();
+// Adapted from georgepapadakis.me/demo/expanding-textarea.html
+(function(){
+  
+  var textareas = document.querySelectorAll('.expanding'),
+      
+      resize = function(t) {
+        t.style.height = 'auto';
+        t.style.overflow = 'hidden'; // Ensure scrollbar doesn't interfere with the true height of the text.
+        t.style.height = (t.scrollHeight + t.offset ) + 'px';
+        t.style.overflow = '';
+      },
+      
+      attachResize = function(t) {
+        if ( t ) {
+          console.log('t.className',t.className);
+          t.offset = !window.opera ? (t.offsetHeight - t.clientHeight) : (t.offsetHeight + parseInt(window.getComputedStyle(t, null).getPropertyValue('border-top-width')));
+
+          resize(t);
+
+          if ( t.addEventListener ) {
+            t.addEventListener('input', function() { resize(t); });
+            t.addEventListener('mouseup', function() { resize(t); }); // set height after user resize
+          }
+
+          t['attachEvent'] && t.attachEvent('onkeyup', function() { resize(t); });
+        }
+      };
+  
+  // IE7 support
+  if ( !document.querySelectorAll ) {
+  
+    function getElementsByClass(searchClass,node,tag) {
+      var classElements = new Array();
+      node = node || document;
+      tag = tag || '*';
+      var els = node.getElementsByTagName(tag);
+      var elsLen = els.length;
+      var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+      for (i = 0, j = 0; i < elsLen; i++) {
+        if ( pattern.test(els[i].className) ) {
+          classElements[j] = els[i];
+          j++;
+        }
+      }
+      return classElements;
+    }
+    
+    textareas = getElementsByClass('expanding');
+  }
+  
+  for (var i = 0; i < textareas.length; i++ ) {
+    attachResize(textareas[i]);
+  }
+
+    $(function() {
+        $(document).scroll(function(){
+            var $nav = $("#mainNavbar");
+            $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        })
+    })
+})();
